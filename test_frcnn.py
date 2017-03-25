f97b8519cd7bad9925fa7561d5cb0c7985b09ca4
Daniel Kang <daniel.d.kang@gmail.com>
2017-03-24 17:13:27 -0700

    Tabs -> spaces

--- test_frcnn.py
+++ test_frcnn.py
@@ -17,31 +17,31 @@ C.use_vertical_flips = False
 
 
 def format_img(img):
-	img_min_side = 600.0
-	(height,width,_) = img.shape
-	
-	if width <= height:
-		f = img_min_side/width
-		new_height = int(f * height)
-		new_width = int(img_min_side)
-	else:
-		f = img_min_side/height
-		new_width = int(f * width)
-		new_height = int(img_min_side)
-	img = cv2.resize(img,(new_width,new_height), interpolation=cv2.INTER_CUBIC)
-	img = img[:,:,(2,1,0)]
-	img = np.transpose(img,(2,0,1)).astype(np.float32)
-	img = np.expand_dims(img, axis=0)
-	img[:, 0, :, :] -= 103.939
-	img[:, 1, :, :] -= 116.779
-	img[:, 2, :, :] -= 123.68
-	return img
+    img_min_side = 600.0
+    (height,width,_) = img.shape
+    
+    if width <= height:
+        f = img_min_side/width
+        new_height = int(f * height)
+        new_width = int(img_min_side)
+    else:
+        f = img_min_side/height
+        new_width = int(f * width)
+        new_height = int(img_min_side)
+    img = cv2.resize(img,(new_width,new_height), interpolation=cv2.INTER_CUBIC)
+    img = img[:,:,(2,1,0)]
+    img = np.transpose(img,(2,0,1)).astype(np.float32)
+    img = np.expand_dims(img, axis=0)
+    img[:, 0, :, :] -= 103.939
+    img[:, 1, :, :] -= 116.779
+    img[:, 2, :, :] -= 123.68
+    return img
 
 with open('classes.json', 'r') as class_data_json:
     class_mapping = json.load(class_data_json)
 
 if 'bg' not in class_mapping:
-	class_mapping['bg'] = len(class_mapping)
+    class_mapping['bg'] = len(class_mapping)
 
 class_mapping = {v: k for k, v in class_mapping.iteritems()}
 
@@ -55,11 +55,11 @@ from keras.models import Model
 from keras_frcnn import roi_helpers
 
 if K.image_dim_ordering() == 'th':
-	input_shape_img = (3, None, None)
-	input_shape_features = (1024, None, None)
+    input_shape_img = (3, None, None)
+    input_shape_features = (1024, None, None)
 else:
-	input_shape_img = (None, None, 3)
-	input_shape_features = (None, None, 1024)
+    input_shape_img = (None, None, 3)
+    input_shape_features = (None, None, 1024)
 
 
 img_input = Input(shape=input_shape_img)
@@ -102,92 +102,92 @@ img_path = sys.argv[1]
 bufsize = 0
 
 for idx,img_name in enumerate(sorted(os.listdir(img_path))):
-	print(img_name)
-	filepath = os.path.join(img_path,img_name)
-	img = cv2.imread(filepath)
+    print(img_name)
+    filepath = os.path.join(img_path,img_name)
+    img = cv2.imread(filepath)
 
-	X = format_img(img)
+    X = format_img(img)
 
-	img_scaled = np.transpose(X[0,(2,1,0),:,:],(1,2,0)).copy()
-	img_scaled[:, :, 0] += 123.68
-	img_scaled[:, :, 1] += 116.779
-	img_scaled[:, :, 2] += 103.939
-	
-	img_scaled = img_scaled.astype(np.uint8)
+    img_scaled = np.transpose(X[0,(2,1,0),:,:],(1,2,0)).copy()
+    img_scaled[:, :, 0] += 123.68
+    img_scaled[:, :, 1] += 116.779
+    img_scaled[:, :, 2] += 103.939
+    
+    img_scaled = img_scaled.astype(np.uint8)
 
-	if K.image_dim_ordering() == 'tf':
-		X = np.transpose(X,(0,2,3,1))
-	# get the feature maps and output from the RPN
-	[Y1, Y2, F] = model_rpn.predict(X)
+    if K.image_dim_ordering() == 'tf':
+        X = np.transpose(X,(0,2,3,1))
+    # get the feature maps and output from the RPN
+    [Y1, Y2, F] = model_rpn.predict(X)
 
-	R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_dim_ordering())
+    R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_dim_ordering())
 
-	# convert from (x1,y1,x2,y2) to (x,y,w,h)
-	R[:,2] = R[:,2] - R[:,0]
-	R[:,3] = R[:,3] - R[:,1]
+    # convert from (x1,y1,x2,y2) to (x,y,w,h)
+    R[:,2] = R[:,2] - R[:,0]
+    R[:,3] = R[:,3] - R[:,1]
 
-	# apply the spatial pyramid pooling to the proposed regions
-	bboxes = {}
-	probs = {}
-	for jk in range(R.shape[0]//num_rois + 1):
-		ROIs = np.expand_dims(R[num_rois*jk:num_rois*(jk+1),:],axis=0)
-		if ROIs.shape[1] == 0:
-			break
+    # apply the spatial pyramid pooling to the proposed regions
+    bboxes = {}
+    probs = {}
+    for jk in range(R.shape[0]//num_rois + 1):
+        ROIs = np.expand_dims(R[num_rois*jk:num_rois*(jk+1),:],axis=0)
+        if ROIs.shape[1] == 0:
+            break
 
-		if jk == R.shape[0]//num_rois:
-			#pad R
-			curr_shape = ROIs.shape
-			target_shape = (curr_shape[0],num_rois,curr_shape[2])
-			ROIs_padded = np.zeros(target_shape).astype(ROIs.dtype)
-			ROIs_padded[:,:curr_shape[1],:] = ROIs
-			ROIs_padded[0,curr_shape[1]:,:] = ROIs[0,0,:]
-			ROIs = ROIs_padded
+        if jk == R.shape[0]//num_rois:
+            #pad R
+            curr_shape = ROIs.shape
+            target_shape = (curr_shape[0],num_rois,curr_shape[2])
+            ROIs_padded = np.zeros(target_shape).astype(ROIs.dtype)
+            ROIs_padded[:,:curr_shape[1],:] = ROIs
+            ROIs_padded[0,curr_shape[1]:,:] = ROIs[0,0,:]
+            ROIs = ROIs_padded
 
-		[P_cls, P_regr] = model_classifier.predict([F, ROIs])
-		P_regr = P_regr / C.std_scaling
+        [P_cls, P_regr] = model_classifier.predict([F, ROIs])
+        P_regr = P_regr / C.std_scaling
 
-		for ii in range(P_cls.shape[1]):
+        for ii in range(P_cls.shape[1]):
 
-			if np.max(P_cls[0,ii,:]) < 0.5 or np.argmax(P_cls[0,ii,:]) == (P_cls.shape[2] - 1):
-				continue
+            if np.max(P_cls[0,ii,:]) < 0.5 or np.argmax(P_cls[0,ii,:]) == (P_cls.shape[2] - 1):
+                continue
 
-			cls_name = class_mapping[np.argmax(P_cls[0,ii,:])]
+            cls_name = class_mapping[np.argmax(P_cls[0,ii,:])]
 
-			if cls_name not in bboxes:
-				bboxes[cls_name] = []
-				probs[cls_name] = []
+            if cls_name not in bboxes:
+                bboxes[cls_name] = []
+                probs[cls_name] = []
 
-			(x, y, w, h) = ROIs[0,ii,:]
+            (x, y, w, h) = ROIs[0,ii,:]
 
-			cls_num = np.argmax(P_cls[0, ii, :])
-			(tx, ty, tw, th) = P_regr[0, ii, 4*cls_num:4*(cls_num+1)]
-			x, y, w, h = roi_helpers.apply_regr(x, y, w, h, tx, ty, tw, th)
+            cls_num = np.argmax(P_cls[0, ii, :])
+            (tx, ty, tw, th) = P_regr[0, ii, 4*cls_num:4*(cls_num+1)]
+            x, y, w, h = roi_helpers.apply_regr(x, y, w, h, tx, ty, tw, th)
 
-			bboxes[cls_name].append([16*x, 16*y, 16*(x+w), 16*(y+h)])
-			probs[cls_name].append(np.max(P_cls[0, ii, :]))
+            bboxes[cls_name].append([16*x, 16*y, 16*(x+w), 16*(y+h)])
+            probs[cls_name].append(np.max(P_cls[0, ii, :]))
 
-	all_dets = {}
+    all_dets = {}
 
-	for key in bboxes:
-		bbox = np.array(bboxes[key])
+    for key in bboxes:
+        bbox = np.array(bboxes[key])
 
-		new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlapThresh=0.5)
-		for jk in range(new_boxes.shape[0]):
-			(x1,y1,x2,y2) = new_boxes[jk,:]
+        new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlapThresh=0.5)
+        for jk in range(new_boxes.shape[0]):
+            (x1,y1,x2,y2) = new_boxes[jk,:]
 
-			cv2.rectangle(img_scaled,(x1, y1), (x2, y2), class_to_color[key],2)
+            cv2.rectangle(img_scaled,(x1, y1), (x2, y2), class_to_color[key],2)
 
-			textLabel = '{}:{}'.format(key,int(100*new_probs[jk]))
-			if key not in all_dets:
-				all_dets[key] = 100*new_probs[jk]
-			else:
-				all_dets[key] = max(all_dets[key],100*new_probs[jk])
+            textLabel = '{}:{}'.format(key,int(100*new_probs[jk]))
+            if key not in all_dets:
+                all_dets[key] = 100*new_probs[jk]
+            else:
+                all_dets[key] = max(all_dets[key],100*new_probs[jk])
 
-			(retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
-			textOrg = (x1,y1+20)
+            (retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
+            textOrg = (x1,y1+20)
 
-			#cv2.rectangle(img_scaled,(textOrg[0] - 5,textOrg[1]+baseLine - 5),(textOrg[0]+retval[0] + 5,textOrg[1]-retval[1] - 5),(0,0,0),2)
-			#cv2.rectangle(img_scaled,(textOrg[0] - 5,textOrg[1]+baseLine - 5),(textOrg[0]+retval[0] + 5,textOrg[1]-retval[1] - 5),(255,255,255),-1)
-			#cv2.putText(img_scaled,textLabel,textOrg,cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),1)
-	cv2.imshow('img',img_scaled)
-	cv2.waitKey(0)
+            #cv2.rectangle(img_scaled,(textOrg[0] - 5,textOrg[1]+baseLine - 5),(textOrg[0]+retval[0] + 5,textOrg[1]-retval[1] - 5),(0,0,0),2)
+            #cv2.rectangle(img_scaled,(textOrg[0] - 5,textOrg[1]+baseLine - 5),(textOrg[0]+retval[0] + 5,textOrg[1]-retval[1] - 5),(255,255,255),-1)
+            #cv2.putText(img_scaled,textLabel,textOrg,cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),1)
+    cv2.imshow('img',img_scaled)
+    cv2.waitKey(0)
--- train_frcnn.py
+++ train_frcnn.py
@@ -16,11 +16,11 @@ from keras_frcnn.pascal_voc_parser import get_data
 all_imgs, classes_count, class_mapping = get_data(sys.argv[1])
 
 if 'bg' not in classes_count:
-	classes_count['bg'] = 0
-	class_mapping['bg'] = len(class_mapping)
+    classes_count['bg'] = 0
+    class_mapping['bg'] = len(class_mapping)
 
 with open('classes.json', 'w') as class_data_json:
-	json.dump(class_mapping, class_data_json)
+    json.dump(class_mapping, class_data_json)
 
 inv_map = {v: k for k, v in class_mapping.iteritems()}
 
@@ -52,9 +52,9 @@ from keras_frcnn import losses
 from keras.callbacks import ReduceLROnPlateau
 
 if K.image_dim_ordering() == 'th':
-	input_shape_img = (3, None, None)
+    input_shape_img = (3, None, None)
 else:
-	input_shape_img = (None, None, 3)
+    input_shape_img = (None, None, 3)
 
 img_input = Input(shape=input_shape_img)
 
@@ -75,11 +75,11 @@ model = Model([img_input, roi_input], rpn + classifier)
 
 
 try:
-	print 'loading weights from ', C.base_net_weights
-	model.load_weights(C.base_net_weights,by_name=True)
+    print 'loading weights from ', C.base_net_weights
+    model.load_weights(C.base_net_weights,by_name=True)
 except:
-	print('Could not load pretrained model weights. Weights can be found at {} and {}'.format(
-		'https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_th_dim_ordering_th_kernels_notop.h5',
+    print('Could not load pretrained model weights. Weights can be found at {} and {}'.format(
+        'https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_th_dim_ordering_th_kernels_notop.h5',
         'https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
         ))
 
@@ -89,14 +89,14 @@ model.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losse
 nb_epochs = 100
 
 callbacks = [EarlyStopping(monitor='val_loss', patience=20, verbose=0),
-				ModelCheckpoint(C.model_path, monitor='val_loss', save_best_only=True, verbose=0),
-			 ReduceLROnPlateau(monitor='loss', factor=0.1, patience=5, min_lr=1e-7, verbose=1)]
+                ModelCheckpoint(C.model_path, monitor='val_loss', save_best_only=True, verbose=0),
+             ReduceLROnPlateau(monitor='loss', factor=0.1, patience=5, min_lr=1e-7, verbose=1)]
 train_samples_per_epoch = 500  #len(train_imgs)
 nb_val_samples = 100  # len(val_imgs),
 
 print 'Starting training'
 
 model.fit_generator(data_gen_train, steps_per_epoch=train_samples_per_epoch, epochs= nb_epochs,
-					validation_data=data_gen_val, validation_steps=nb_val_samples, callbacks=callbacks,
-					max_q_size=1, workers=1)
+                    validation_data=data_gen_val, validation_steps=nb_val_samples, callbacks=callbacks,
+                    max_q_size=1, workers=1)
 
